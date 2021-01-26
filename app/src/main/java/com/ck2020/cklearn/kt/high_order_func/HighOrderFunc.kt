fd2d5fa2c75429936b31dcc2ee5ca06f6202e122
chenke <chen.ke@yanwei365.com>
2021-01-26 17:25:40 +0800

    高阶函数

--- app/src/main/java/com/ck2020/cklearn/kt/high_order_func/HighOrderFunc.kt
+++ app/src/main/java/com/ck2020/cklearn/kt/high_order_func/HighOrderFunc.kt
@@ -1,11 +1,41 @@
 package com.ck2020.cklearn.kt.high_order_func
 
+import com.blankj.utilcode.util.ToastUtils
+
 /**
  * @author chenke
  * @create 2021/1/25
  * @Describe
  * 高阶函数
+ * 概念，如果一个函数，支持入参为函数类型的参数
+ * 即该函数为高阶函数:写法
+ * (a:type,b:type)->return_type
+ * (a:Int,b:Int)->String
+ * a:入参，类型int
+ * b:入参，类型int
+ * ->String:函数类型参数返回值
  */
-class HighOrderFunc {
+object HighOrderFunc {
+    /**
+     * 加
+     */
+    fun plus(a: Int, b: Int): Int {
+        return a.plus(b)
+    }
 
+    /**
+     * 减
+     */
+    fun minus(a: Int, b: Int): Int {
+        return a.minus(b)
+    }
+
+    /**
+     * a:普通入参String
+     * b:普通入参Int
+     * func:函数类型入参Func
+     */
+    fun highOrderFuncSimple(a: Int, b: Int, func: (Int, Int) -> Int): String {
+        return "a入参:${a}\nb入参${b}\n函数计算结果:${func.invoke(a, b)}"
+    }
 }
--- app/src/main/java/com/ck2020/cklearn/kt/high_order_func/TestHighOrderActivity.kt
+++ app/src/main/java/com/ck2020/cklearn/kt/high_order_func/TestHighOrderActivity.kt
@@ -2,11 +2,58 @@ package com.ck2020.cklearn.kt.high_order_func
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blankj.utilcode.util.ToastUtils
 import com.ck2020.cklearn.R
+import com.ck2020.cklearn.databinding.ActivityHighOrderBinding
 
+/**
+ * 高阶函数
+ */
 class TestHighOrderActivity : AppCompatActivity() {
+    private lateinit var mHighOrderBind: ActivityHighOrderBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_high_order)
+        mHighOrderBind = ActivityHighOrderBinding.inflate(layoutInflater)
+        setContentView(mHighOrderBind.root)
+        mHighOrderBind.btPlus.setOnClickListener {
+            //执行plus函数
+            val a = mHighOrderBind.etHighA.text.toString().toInt()
+            val b = mHighOrderBind.etHighB.text.toString().toInt()
+            val c = mHighOrderBind.etNumberC.text.toString().toInt()
+            val d = mHighOrderBind.etNumberD.text.toString().toInt()
+            val result = HighOrderFunc.highOrderFuncSimple(a, b) { _, _ ->
+                c + d
+            }
+            val result_ = HighOrderFunc.highOrderFuncSimple(a, b, ::plus)
+            ToastUtils.showShort(result_)
+            mHighOrderBind.tvResult.text = "$result"
+        }
+        mHighOrderBind.btMinus.setOnClickListener {
+            //执行minus函数
+            val a = mHighOrderBind.etHighA.text.toString().toInt()
+            val b = mHighOrderBind.etHighB.text.toString().toInt()
+            val c = mHighOrderBind.etNumberC.text.toString().toInt()
+            val d = mHighOrderBind.etNumberD.text.toString().toInt()
+            val result = HighOrderFunc.highOrderFuncSimple(a, b) { _, _ ->
+                c - d
+            }
+            mHighOrderBind.tvResult.text = "$result"
+            val result_ = HighOrderFunc.highOrderFuncSimple(a, b, ::minus)
+            ToastUtils.showShort(result_)
+        }
+    }
+
+    /**
+     * 加
+     */
+    fun plus(a: Int, b: Int): Int {
+        return a.plus(b)
+    }
+
+    /**
+     * 减
+     */
+    fun minus(a: Int, b: Int): Int {
+        return a.minus(b)
     }
 }
