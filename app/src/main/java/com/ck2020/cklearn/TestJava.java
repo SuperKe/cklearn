bc17bf326c271395d03b2c10071c7f4ef622eb31
chenke <chen.ke@yanwei365.com>
2021-02-01 17:29:17 +0800

    泛型擦除测试

--- app/src/main/java/com/ck2020/cklearn/TestJava.java
+++ app/src/main/java/com/ck2020/cklearn/TestJava.java
@@ -1,5 +1,8 @@
 package com.ck2020.cklearn;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+
 /**
  * @author chenke
  * @create 2021/1/27
@@ -10,6 +13,18 @@ class TestJava {
         String str1 = "123", str2 = "123";
         System.out.println(str1.equals(str2));
         System.out.println(str1 == str2);
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        //这样调用 add 方法只能存储整形，因为泛型类型的实例为 Integer
+        try {
+            list.getClass().getMethod("add", Object.class).invoke(list, "asd");
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
     }
 
     private void methodA(final String test) {
