d9ab46b34a22d677e2428bfb7849aea0330136e6
chenke <chen.ke@yanwei365.com>
2021-02-24 16:15:39 +0800

    为kotlin lazy指定抽象规则

--- app/src/main/java/com/ck2020/cklearn/lazy/kt/LazyFragmentKt.kt
+++ app/src/main/java/com/ck2020/cklearn/lazy/kt/LazyFragmentKt.kt
@@ -1,5 +1,9 @@
 package com.ck2020.cklearn.lazy.kt
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 
@@ -18,12 +22,27 @@ public abstract class LazyFragmentKt : Fragment() {
 
     // 数据是否已请求
     private var isDataLoaded = false
+    public abstract fun getLayout(): Int
+    public abstract fun initView(view: View)
 
     /**
      * 第一次可见时触发调用,此处实现具体的数据请求逻辑
      */
     public fun onStartLoad() {
 
+    }
+
+    private var mRootView: View? = null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        if (mRootView == null) {
+            mRootView = inflater.inflate(getLayout(), container, false)
+        }
+        return mRootView
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initView(mRootView!!)
     }
 
 
